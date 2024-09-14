f71e178bd6beab8773e8e37f3b3f8cfeb65478d7
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-09-14 19:21:21 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,6 +33,7 @@ class FileStorage:
         """Serialize __objects to the JSON file __file_path."""
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+         print("objdict:", objdict)
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
 
