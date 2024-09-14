a7a1afc4ff4104bc73a913fcbbcdce16125acb24
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-09-14 17:43:07 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Module file_storage
+"""Module file_storage.
 
-This Module contains a definition for FileStorage Class
+This Module contains a definition for FileStorage Class.
 """
-
-
 import importlib
 import json
 import os
@@ -12,39 +10,55 @@ import re
 
 
 class FileStorage:
-    """FileStorage Class
+    """FileStorage Class.
+
+    This class manages storage of hbnb models in JSON format.
 
     Attributes:
-        __file_path (str): string - path to the JSON file
-        __objects (dict): A dictionary of instantiated objects.
-
+        __file_path (str): Path to the JSON file.
+        __objects (dict): Dictionary of instantiated objects.
     """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
+        """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        """Set in __objects obj with key <obj_class_name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
+        json_objects = {
+            key: val.to_dict() for key, val in self.__objects.items()
+        }
         with open(self.__file_path, 'w') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+            json.dump(json_objects, f)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
-        if (os.path.isfile(self.__file_path)
-                and os.path.getsize(self.__file_path) > 0):
+        try:
             with open(self.__file_path, 'r') as f:
-                self.__objects = {k: self.get_class(k.split(".")[0])(**v)
-                                  for k, v in json.load(f).items()}
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = self.get_class(
+                    key.split('.')[0])(**jo[key])
+        except FileNotFoundError:
+            pass
 
     def get_class(self, name):
-        """ returns a class from models module using its name"""
+        """Return a class from models module using its name.
+
+        Args:
+            name (str): The name of the class.
+
+        Returns:
+            type: The class.
+        """
         sub_module = re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
         module = importlib.import_module(f"models.{sub_module}")
         return getattr(module, name)
