69f18df4054b64e72dd95618d00992942e563f6d
Relebohile.P <r.pheko@alustudent.com>
2024-09-14 04:42:48 -0700

    update console file

--- console.py
+++ console.py
@@ -2,12 +2,15 @@
 """Module for the entry point of the command interpreter."""
 
 import cmd
-
+import shlex
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class."""
 
     prompt = "(hbnb) "
+    __classes = {"BaseModel"}
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -15,21 +18,128 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """EOF command to exit the program."""
-        print()  # Print a newline for a clean exit
+        print()
         return True
 
     def emptyline(self):
         """Do nothing on empty line."""
         pass
 
-    def help_quit(self):
-        """Help message for the quit command."""
-        print("Quit command to exit the program")
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id."""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
 
-    def help_EOF(self):
-        """Help message for the EOF command."""
-        print("EOF command to exit the program")
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+        else:
+            print(storage.all()[key])
 
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()[key]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances."""
+        args = shlex.split(arg)
+        obj_list = []
+        if not args:
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+        elif args[0] in self.__classes:
+            for key, obj in storage.all().items():
+                if key.split('.')[0] == args[0]:
+                    obj_list.append(str(obj))
+        else:
+            print("** class doesn't exist **")
+            return
+        print(obj_list)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        obj = storage.all()[key]
+        setattr(obj, args[2], args[3].strip('"'))
+        obj.save()
+
+    def help_create(self):
+        """Help message for create command."""
+        print("Creates a new instance of BaseModel, saves it and prints the id.")
+        print("Usage: create <class name>")
+
+    def help_show(self):
+        """Help message for show command."""
+        print("Prints the string representation of an instance.")
+        print("Usage: show <class name> <id>")
+
+    def help_destroy(self):
+        """Help message for destroy command."""
+        print("Deletes an instance based on the class name and id.")
+        print("Usage: destroy <class name> <id>")
+
+    def help_all(self):
+        """Help message for all command."""
+        print("Prints all string representation of all instances.")
+        print("Usage: all [<class name>]")
+
+    def help_update(self):
+        """Help message for update command."""
+        print("Updates an instance based on the class name and id.")
+        print("Usage: update <class name> <id> <attribute name> \"<attribute value>\"")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
